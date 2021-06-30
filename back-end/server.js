68ccaf5ffcd49c353537d85cd89116955a7bac6c
Panagiotis.Kolaitis <panagiotis.kolaitis.ext@proximus.com>
2021-07-01 00:31:48 +0200

    event emitter

--- back-end/server.js
+++ back-end/server.js
@@ -4,14 +4,38 @@ const cors = require('cors');
 const CONFIG = require('./config/config');
 const express = require( 'express' );
 const path = require( 'path' );
+const workers = [];
+var numReqs = 0;
 
 var root = path.dirname( __dirname );
 console.log(CONFIG);
 if( cluster.isMaster ) {
   // Create a worker for each CPU
-  for( var i = 0; i < CONFIG.REPLICAS; i++ ) {
-    cluster.fork();
+  var broadcast = function() {
+    for (var i in workers) {
+      var worker = workers[i];
+      worker.send({ cmd: 'broadcast', numReqs: numReqs });
+    }
   }
+  for( var i = 0; i < CONFIG.REPLICAS; i++ ) {
+    workers.push(cluster.fork());
+    workers[i].on('message', function(msg) {
+      if (msg.cmd) {
+        switch (msg.cmd) {
+          case 'notifyRequest':
+            numReqs++;
+          break;
+          case 'broadcast':
+            broadcast();
+          break;
+        }
+      }
+    });
+  }
+  
+  setInterval(function() {
+    console.log("numReqs =", numReqs);
+  }, 1000);
 
   cluster.on( 'online', function( worker ) {
     console.log( 'Worker ' + worker.process.pid + ' is online.' );
@@ -21,18 +45,38 @@ if( cluster.isMaster ) {
   });
 }
 else {
-  const app    = express();
-//   var routes = require( './routes' )( app );
-    app.get('/', cors(), (req, res) => {
-        setTimeout(()=>{
-            const result = {msg: 'Worker ' + process.pid + ' responded.' };
-            console.log(result);
-            res.send(result);
-        }, CONFIG.DDELAY);
-        
-    } );
-  app
-    .use(cors())
-    .listen( CONFIG.PORT);
+  const app = express();
+  cluster.workerData = {
+    id: process.pid,
+    greeting: 'Worker ' + process.pid + ' responded'
+  };
+
+  process.on('message', function(msg) {
+    switch(msg.cmd) {
+      case 'broadcast':
+        if (msg.numReqs) console.log(process.pid + 'Number of requests: ' + msg.numReqs);
+      break;
+    }
+  });
+
+  app.get('/', cors(), (req, res) => {
+    // cluster.send('message', message);
+    process.send({ cmd: 'notifyRequest' });
+    process.send({ cmd: 'broadcast' });
+    setTimeout(()=>{
+        const result = cluster.workerData;
+        console.log(result);
+        res.send(result);
+    }, CONFIG.DDELAY);  
+  });
+
+  app.use(cors()).listen( CONFIG.PORT);
+
+  cluster.addListener('message', function(data){
+    console.log('Worker ' + process.pid + ' received data:' + data.message + data.id);
+  });
+  const message = {message: 'Worker ' + process.pid + ' responded.', id: process.pid };
+  cluster.emit('message', message);
+  
 
 }
